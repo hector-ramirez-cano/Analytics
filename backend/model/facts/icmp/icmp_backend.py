343c9105adfb43d6a0d9ed64e3431d486d5f7ec6
Héctor Miguel Ramírez Cano <hmramirezcano@gmail.com>
2025-10-28 09:21:17 -0600

    [BACKEND] Fixed ICMP data extaraction patterns

--- backend/model/facts/icmp/icmp_backend.py
+++ backend/model/facts/icmp/icmp_backend.py
@@ -7,8 +7,8 @@ from model.cache import Cache
 import asyncio
 
 
-RTT_PATTERN = re.compile(r"rtt min/avg/max/mdev = .*/(.*)/.*/.*? ms")
-LOSS_PATTERN = re.compile(r"(\d\d?\d?)% packet loss")
+RTT_PATTERN = re.compile(r"rtt min/avg/max/mdev = ([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+) ms")
+LOSS_PATTERN = re.compile(r"(\d+(?:\.\d+)?)% packet loss")
 UNREACHABLE_PATTERN = "Destination Host Unreachable"
 NAME_RESOLUTION_ERROR_PATTERN = "Temporary failure in name resolution"
 TIMEOUT_ERROR_PATTERN = "Request Timed Out"
@@ -18,7 +18,7 @@ HOST_NOT_FOUND_PATTERN = "Could Not Find Host"
 def __ping_host_once(host: str):
     # TODO: Switch to raw ICMP sockets for faster throughput
     result = subprocess.run(
-        ['ping', '-A', '-c', '1', host],
+        ['ping', '-A', '-c', '3', host],
         capture_output=True,
         text=True,
         check=False,
@@ -29,9 +29,9 @@ def __ping_host_once(host: str):
     status = IcmpStatus.UNREACHABLE
 
     if result.returncode == 0:
-        rtt  = float(re.findall(RTT_PATTERN, result.stdout)[0])
-        loss = float(re.findall(LOSS_PATTERN, result.stdout)[0])
-        status = IcmpStatus.REACHABLE
+        rtt      = __parse_float(re.findall(RTT_PATTERN, result.stdout)[0][1], 0.0)
+        loss     = __parse_float(re.findall(LOSS_PATTERN, result.stdout)[0], 0.0)
+        status   = IcmpStatus.REACHABLE
 
     else:
         if UNREACHABLE_PATTERN in result.stderr:
@@ -49,6 +49,11 @@ def __ping_host_once(host: str):
 
     return result.returncode, host, status, rtt, loss
 
+def __parse_float(value: str, default: float):
+    try:
+        return float(value)
+    except ValueError as _:
+        return default
 
 async def __ping_devices(hosts) -> tuple[dict, dict]:
     """
@@ -67,7 +72,7 @@ async def __ping_devices(hosts) -> tuple[dict, dict]:
         metrics[host] = {
             "icmp_status" : icmp_status,
             "icmp_rtt": rtt,
-            "icmp_loss_percent": loss
+            "icmp_loss_percent": loss,
         }
 
         status[host] = {"icmp_status": {"status": icmp_status, "msg": ""}}
